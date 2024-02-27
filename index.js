c9e9dcff1f7a00b76114d4e470c2b92bb4524386
SangleMahesh <maheshsangle431@gmail.com>
2024-02-27 18:36:46 +0530

    updated index and package

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-const express = require("express");
 const dotenv = require("dotenv");
-const e = require("express");
 dotenv.config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const staticRouter = require("./Routes/staticRouter");
+const signupRouter = require("./Routes/signupRouter");
 
 const app = express();
-
 const port = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => console.log("Database connected successfully"));
+
+// Middlewares
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.set("view engine", "ejs");
+
+// Routes
+app.use("/", staticRouter);
+app.use("/signup", signupRouter);
 
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
