fcba924dd3f534581aefd27bb4b529f7ac611a4b
SangleMahesh <maheshsangle431@gmail.com>
2024-02-29 16:42:47 +0530

    updated signupRouter

--- Routes/signupRouter.js
+++ Routes/signupRouter.js
@@ -2,6 +2,6 @@ const express = require("express");
 const handleSignup = require("../Controllers/signupHandler");
 const signupRouter = express.Router();
 
-signupRouter.post("/", handleSignup);
+signupRouter.post("/", validateUserData, handleSignup);
 
 module.exports = signupRouter;
